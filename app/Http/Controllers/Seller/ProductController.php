5a7fb51c39465e4cde2a992b104239472e96a9e4
loulou <louismorganf@gmail.com>
2024-12-08 10:28:00 +0700

    migrations

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
 }
