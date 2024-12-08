fb63d2e704d5bdec3cfa756a57653dc351b8d9d5
loulou <louismorganf@gmail.com>
2024-12-08 11:20:31 +0700

    users

--- database/migrations/2024_12_08_032604_create_users_table.php
+++ database/migrations/2024_12_08_032604_create_users_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
+class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Buyer
-            $table->foreignId('product_id')->constrained()->onDelete('cascade'); // Product in the cart
-            $table->integer('quantity')->default(1);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['buyer', 'seller', 'admin'])->default('buyer');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('users');
     }
 }
